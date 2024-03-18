8d86faa2f5313894213a71af7f30839761d53acd
MariaRantanen <48141@edu.raseko.fi>
2024-03-18 11:28:22 +0200

    Frankku_perkele

--- expo-app/App.js
+++ expo-app/App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/images/Frankku.jpg');
+
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="choose a photo " />
+        <Button label="Use this photo" onPress={pickImageAsync} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +43,16 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  flex: 1,
+  backgroundColor: '#25292e',
+  alignItems: 'center',
+},
+  imageContainer: {
+  flex: 1,
+  paddingTop: 58,
+},
+footerContainer: {
+  flex: 1 / 3,
+  alignItems: 'center',
+},
 });
